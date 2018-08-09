67d718012e5f65a2087bdf7e664b8d886554de23
voya <voyax3@gmail.com>
2018-08-09 13:05:55 +0800

    feat: add toc support

--- source-src/js/index.js
+++ source-src/js/index.js
@@ -2,3 +2,11 @@ import '../style/index.less'
 import hljs from 'highlightjs/highlight.pack.min.js';
 
 hljs.initHighlightingOnLoad();
+
+const tocTip = document.querySelector(".toc-tip");
+function toggleToc() {
+  document.querySelector('.toc-container').classList.toggle('show');
+}
+if (tocTip) {
+  tocTip.addEventListener("mouseenter", toggleToc);
+}
